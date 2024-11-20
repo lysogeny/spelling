615207fb278c06e8c783b1c6ea24576e836c1f74
Aada <code@jooa.xyz>
2024-11-20 19:22:40 +0100

    Fix code issues

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,30 @@
+use encoding::{all::ISO_8859_1, EncoderTrap, Encoding};
 use hunspell_rs::Hunspell;
-use hunspell_sys::{
-    Hunspell_create,
-    Hunspell_spell,
-};
-use encoding::{all::ISO_8859_1, Encoding, EncoderTrap};
+use hunspell_sys::{Hunspell_create, Hunspell_get_dic_encoding, Hunspell_spell};
 
-const affix_path: &str = "lang/de_DE.aff";
-const dic_path: &str = "lang/de_DE.dic";
+const AFFIX_PATH: &str = "lang/de_DE.aff";
+const DIC_PATH: &str = "lang/de_DE.dic";
 
 unsafe fn check_with_encoding(word: &str) -> bool {
-    let hs = Hunspell_create(
-        std::ffi::CString::new(affix_path).unwrap().as_ptr(),
-        std::ffi::CString::new(dic_path).unwrap().as_ptr(),
-    );
+    let affix_path = std::ffi::CString::new(AFFIX_PATH).unwrap();
+    let dic_path = std::ffi::CString::new(DIC_PATH).unwrap();
+    let hs = Hunspell_create(affix_path.as_ptr(), dic_path.as_ptr());
     let word_enc: &[u8] = &ISO_8859_1.encode(word, EncoderTrap::Strict).unwrap();
-    Hunspell_spell(hs, std::ffi::CString::new(word_enc).unwrap().as_ptr()) > 0
+    let word_c = std::ffi::CString::new(word_enc).unwrap();
+    Hunspell_spell(hs, word_c.as_ptr()) > 0
 }
 
 fn check_binding(word: &str) -> bool {
-    let spell = Hunspell::new(affix_path, dic_path);
+    let spell = Hunspell::new(AFFIX_PATH, DIC_PATH);
     spell.check(word) == hunspell_rs::CheckResult::FoundInDictionary
 }
 
 fn main() {
-    let words = vec![
-        "Öl", 
-        "Baum",
-    ];
-        println!("{:>8} {:<8} {:<8}", "word", "binding", "with_encoding");
+    let words = vec!["Öl", "Baum"];
+    println!("{:>8} {:<8} {:<8}", "word", "binding", "with_encoding");
     for word in words {
         let result_binding = check_binding(word);
-        let result_encoded = unsafe {check_with_encoding(word)};
+        let result_encoded = unsafe { check_with_encoding(word) };
         println!("{word:>8} {result_binding:<8} {result_encoded:<8}")
     }
 }
